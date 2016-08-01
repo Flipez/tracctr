51bb0a9d0c7e1bdf77c71bbd66f3a36c6fbf31e1
Flipez <github@flipez.net>
2016-08-01 19:19:18 +0000

    add some stats to welcome page
    Signed-off-by: Flipez <github@flipez.net>

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,12 @@ class WelcomeController < ApplicationController
   
   skip_before_filter :authorize
 
-  def index; end
+  def index
+    @user_count = User.count
+    @account_count = Account.count
+    @bill_count = Bill.count
+    @one_shot_count = OneShot.count
+    @bullet_count = Bullet.count
+  end
 
 end
